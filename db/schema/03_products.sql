371b9995040b9d4e30661108edf0bab1f9b4ba94
alisonhussey <alisonhussey@gmail.com>
2021-01-13 17:18:42 +0000

    changes input in products file for photo_url to accept the new seeds

--- db/schema/03_products.sql
+++ db/schema/03_products.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  photo_url VARCHAR(255) NOT NULL,
+  photo_url TEXT NOT NULL,
   price INTEGER  NOT NULL DEFAULT 0,
   color VARCHAR(255) NOT NULL,
   description text,
