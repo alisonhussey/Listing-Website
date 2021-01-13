cee590f2dbdb2295e3d51f9e7a2240300bf5bd3e
alisonhussey <alisonhussey@gmail.com>
2021-01-13 17:51:42 +0000

    finished query to find conversations by product id, and renamed files incorrectly named

---
+++ db/queries/15_find_convos_by_product_id.sql
@@ -0,0 +1,9 @@
+SELECT users.first_name as name, messages.content, messages.time_sent
+FROM messages
+JOIN conversations ON conversations.id = messages.conversation_id
+JOIN products ON products.id = conversations.product_id
+JOIN users ON users.id = user_id
+WHERE product_id = 3
+GROUP BY product_id, users.first_name, messages.content, messages.time_sent
+ORDER BY messages.time_sent;
+
