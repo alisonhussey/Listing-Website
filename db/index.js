a77e08a819a941ce02ca26eca23e6fafb40d1963
alisonhussey <alisonhussey@gmail.com>
2021-01-13 16:35:43 +0000

    re-updated index.js, and queries 14 and 15

--- db/index.js
+++ db/index.js
@@ -6,20 +6,6 @@ const pool = new Pool({
   host: 'localhost',
   database: 'midterm'
 });
-
-const getUserWithEmail = function(email) {
-  const queryString = `
-  SELECT *
-  FROM users
-  WHERE email = $1;
-  `
-  return pool.query(queryString, [email])
-  .then(res => res.rows[0])
-  .catch(err => console.log(err.stack));
-}
-
-exports.getUserWithEmail = getUserWithEmail;
-
 const addUser =  function(user) {
   const queryString = `
   INSERT INTO users (first_name, last_name, email, password, is_admin)
@@ -30,8 +16,33 @@ const addUser =  function(user) {
   return pool.query(queryString, values)
   .then(res => res.rows[0])
   .catch(err => console.log(err.stack));
-}
+};
 exports.addUser = addUser;
+
+const getUserWithEmail = function(email) {
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE email = $1;
+  `
+  return pool.query(queryString, [email])
+  .then(res => res.rows[0])
+  .catch(err => console.log(err.stack));
+};
+exports.getUserWithEmail = getUserWithEmail;
+
+const getUserWithId = function(id) {
+  const queryString = `
+    SELECT *
+    FROM users
+    WHERE id = $1;
+  `;
+  const values = [id];
+  return pool.query(queryString, values)
+    .then(res => res.rows[0] || null)
+    .catch(err => console.log(err.stack));
+};
+exports.getUserWithId = getUserWithId;
 
 const addProduct = function(product) {
   const queryString = `INSERT INTO products (name, photo_url, price, color, description, is_available, user_id, product_category_id)
@@ -54,6 +65,58 @@ const addProduct = function(product) {
 };
 exports.addProduct = addProduct;
 
+const markAsSold = function(product) {
+  const queryString = `
+  UPDATE products
+  SET is_available = false
+  WHERE id = $1;
+  `
+  const values = [product.id];
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log(err.stack));
+};
+exports.markAsSold = markAsSold;
+
+const deleteProduct = function(product) {
+  const queryString = `
+    DELETE FROM products
+    WHERE id = $1;
+  `
+  const values = [product.id];
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log(err.stack));
+};
+exports.deleteProduct = deleteProduct;
+
+const getAllProducts = function() {
+  const queryString = `
+  SELECT *
+  FROM products;
+  `
+  return pool.query(queryString)
+  .then(res => res.rows)
+  .catch(err => console.log(err.stack));
+};
+exports.getAllProducts = getAllProducts;
+
+const getFavouritesByUser = function(user) {
+  const queryString = `
+  SELECT products.name
+  FROM products
+  JOIN favourite_products ON products.id = product_id
+  JOIN users ON users.id = favourite_products.user_id
+  WHERE users.id = $1;
+  `
+  values = [user.id];
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log(err.stack));
+};
+exports.getFavouritesByUser = getFavouritesByUser;
+
+
 const getProductsByCategory = function(category){
   const queryString = `
   SELECT *
@@ -68,3 +131,31 @@ const getProductsByCategory = function(category){
 };
 exports.getProductsByCategory = getProductsByCategory;
 
+const getProductsByPrice = function(category){
+  const queryString = `
+  SELECT *
+  FROM products
+  WHERE price >= $1 AND price <= $2
+  ORDER BY price;
+  `
+  const values = [category.name];
+  return pool.query(queryString, values)
+  .then(res => res.rows)
+  .catch(err => console.log(err.stack));
+};
+exports.getProductsByPrice = getProductsByPrice;
+
+const getConversationsByUserId = function (user) {
+  const queryString = `
+  SELECT users.first_name as name, messages.content, messages.time_sent
+  FROM messages
+  JOIN users ON messages.sender = users.id
+  WHERE sender = 1
+  ORDER BY messages.time_sent;
+  `
+  const values = [user.id];
+  return pool.query(queryString, values)
+  .then(res => res.rows)
+  .catch(err => console.log(err.stack));
+};
+exports.getConversationsByUserId = getConversationsByUserId;
--- db/queries/15_find_convos_by_user_id.sql
+++ db/queries/15_find_convos_by_user_id.sql
@@ -1,5 +1,8 @@
 SELECT users.first_name as name, messages.content, messages.time_sent
-FROM messages
-JOIN users ON messages.sender = users.id
-WHERE sender = 1
+FROM conversations
+JOIN products ON products.id = product_id
+JOIN participants on
+JOIN users ON participants.user_id = users.id
+WHERE product_id = 1
+GROUP BY product_id
 ORDER BY messages.time_sent;
